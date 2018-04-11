417b19030422b8f3b71d20899389ee4f3a23d2d6
Marohn <omarohn@gmail.com>
2018-04-11 20:39:37 +0200

    Wurferwartung

--- src/components/HundKurz.vue
+++ src/components/HundKurz.vue
@@ -5,7 +5,7 @@
     </div>
     <span id="name">{{hund.name}}</span><span id="buch"> - {{hund.zuchtbuch}}</span><span v-if="hund.hd" id="hd"> - HD: {{hund.hd}}</span><span v-if="hund.pra" id="pra"> - PRArcd4: {{hund.pra}}</span><span v-if="hund.ohren" id="ohren"> - Ohren: {{hund.ohren}}</span>
     <div class="pruefung">
-      <property-tag :props="hund.leistung"></property-tag><span id="abl"> - ABL: {{hund.abl | normalize}}</span><span id="ll"> - LL: {{hund.ll | normalize}}</span>
+      <property-tag :props="hund.leistung"></property-tag><span id="abl"> - ABL: {{hund.abl | normalize}}</span><span id="ll"> - LL: {{hund.ll | normalize}}</span><span id="dgstb"> - DGStB: {{hund.dgstb | normalize}}</span>
     </div>
   </div>
 </template>
--- src/components/MainSlide.vue
+++ src/components/MainSlide.vue
@@ -137,6 +137,8 @@
     }
   }
 </script>
+	@FindBy(css="")
+	private WebElement webElement;
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
