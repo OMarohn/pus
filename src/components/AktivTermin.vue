c89cadacfed356a2f601241a995ac9ca69270588
Marohn <omarohn@gmail.com>
2018-05-26 18:05:16 +0200

    typo

--- src/components/AktivTermin.vue
+++ src/components/AktivTermin.vue
@@ -62,7 +62,7 @@
                 </div>
                 <div v-if="theDate.hasOwnProperty('protokoll')">
                   <a data-toggle="tooltip" data-placement="top" title="Protokoll" :href="'static/doc/' + theDate.protokoll" target="prot" class="btn btn-outline-success btn-sm">
-                    <i class="fa fa-file-pdf-o fa-fw"></i><span>Einladung</span>
+                    <i class="fa fa-file-pdf-o fa-fw"></i><span>Protokoll</span>
                   </a>
                 </div>
               </td>
