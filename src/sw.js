a8d1b9a359756a44029a636b79f7b911a2dd6017
Marohn <omarohn@gmail.com>
2018-06-03 18:40:07 +0200

    sw tuning

--- src/sw.js
+++ src/sw.js
@@ -12,10 +12,9 @@ if (workbox) {
     'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff?v=4.7.0',
     'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf?v=4.7.0',
     'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css',
     'https://unpkg.com/animate.css@3.5.1/animate.min.css',
     'https://code.jquery.com/jquery-3.1.1.slim.min.js',
-    'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css',
-    'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     'https://www.google.com/recaptcha/api.js',
     'https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js'
   ];
