9e4f123914ac1813e5f473ed6130a84ed349e9ad
Kai Oliver Marohn <oliver.marohn@gfi.ihk.de>
2020-05-16 17:47:03 +0200

    herbstprüfungen eingetragen und mailaddr geändert

--- upload.php
+++ upload.php
@@ -155,7 +155,7 @@ if (isset($_POST['g-recaptcha-response']) or $skip) {
     $message = json_encode($_POST, JSON_UNESCAPED_UNICODE)."\n"."IP: ".$userIp."\nTimestamp:".time();
 
     // Aufruf der Funktion, Versand
-     $an = "pruefung@pointer-setter-nord.de, admin@pointer-setter-nord.de";
+     $an = "landesgruppenleiter@pointer-setter-nord.de, ausstellung@pointer-setter-nord.de, admin@pointer-setter-nord.de";
     // $an = "admin@pointer-setter-nord.de";
     $sendOK = mail_att($an, "Onlinemeldung - ".$_POST['fuehrer_name'],
       $message,
